7b27f65f940e5e12798c6c4d5411b3942d5c84db
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-15 14:03:20 -0800

    add enforce input to helpers file

--- helpers.py
+++ helpers.py
@@ -3,3 +3,12 @@ def is_between(number, lower_bound, upper_bound):
         return True
     else:
         return False
+
+
+def enforced_input(input_message, valid_input_list):
+    player_input = input(input_message)
+    while player_input not in valid_input_list:
+        print('Invalid command, type help for a list of commands')
+        player_input = input(input_message)
+
+    return player_input
