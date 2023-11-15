ad027c836c7a7154823d157c528e2ed0eecaec22
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-15 14:05:24 -0800

    add strip and lower to input

--- helpers.py
+++ helpers.py
@@ -6,7 +6,7 @@ def is_between(number, lower_bound, upper_bound):
 
 
 def enforced_input(input_message, valid_input_list):
-    player_input = input(input_message)
+    player_input = input(input_message).strip().lower()
     while player_input not in valid_input_list:
         print('Invalid command, type help for a list of commands')
         player_input = input(input_message)
