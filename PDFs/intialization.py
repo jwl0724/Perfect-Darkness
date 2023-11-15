d6ca34d184fe0ef27588019679157ceeb6c1bfb4
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-15 08:58:16 -0800

    finish introduce game

--- PDFs/intialization.py
+++ PDFs/intialization.py
@@ -1,15 +1,20 @@
 import random
 
+
 def introduce_game():
     print("""
-    Welcome to The Stairs
-    
+    Welcome to Perfect Darkness, you are a captured convict sent into a building under sanction. You are tasked 
+    with eliminating the creature confined within the building. The foundation has not provided any tools or weapons 
+    to help in your task, armed with nothing but your wits, you must scour through the building to find any 
+    semblance of equipment to help you in your task. But be careful, the creature is roaming about looking for any 
+    prey that has entered it's domain.
     """)
 
     print('Press enter to continue.')
     print('----------------------------------------------------------------------------')
+
 def make_board(row, height, col):
-    board = {}
+board = {}
     possible_populating_options = [
         ''
     ]
@@ -23,4 +28,4 @@ def make_board(row, height, col):
 
     return board
 
-def create_entity(health, coordinates, speed):
+def create_entity(stats, coordinates, speed, name):
