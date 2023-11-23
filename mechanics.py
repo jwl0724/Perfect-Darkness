8c3f92d425b82c204983da1e4723e68e411dd532
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-23 15:22:13 -0800

    finish process move

--- mechanics.py
+++ mechanics.py
@@ -76,19 +76,24 @@ def process_take(player, board):
 
 def process_move(player, board):
     valid_direction = ('n', 's', 'e', 'w', 'up', 'down', 'help')
+    direction_key = {'n': 1, 's': -1, 'e': -1, 'w': 1, 'up': 1, 'down': -1}
     player_not_moved = True
 
     while player_not_moved:
-        direction = helpers.enforced_input('Which direction would you like to move? (N, S, E, W)', valid_direction)
+        direction = helpers.enforced_input('Which direction would you like to move? (N, S, E, W): ', valid_direction)
+
         if direction == 'help':
             print('Valid directions include: N, S, E, W, Up, Down')
-        if direction == 'up' and board[(player['X'], player['Y'], player['Z'])]['Event'] != 'Stairs':
+        elif direction == 'up' and board[(player['X'], player['Y'], player['Z'])]['Event'] != 'Stairs':
             print('There\'s nothing to climb up from.')
         elif direction == 'down' and board[(player['X'], player['Y'], player['Z'])]['Event'] != 'Hole':
             print('There\'s no hole to jump into.')
         elif helpers.out_of_bounds(player, board, direction):
             print('You moved into a wall... Try again.')
         else:
+            player['X'] += direction_key[direction] if direction == 'e' or direction == 'w' else 0
+            player['Z'] += direction_key[direction] if direction == 'n' or direction == 's' else 0
+            player['Y'] += direction_key[direction] if direction == 'up' or direction == 'down' else 0
             player_not_moved = False
 
 
