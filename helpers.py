a17e5683df645e779988be457d407b33bdfda10d
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-23 14:48:08 -0800

    finish out of bounds check

--- helpers.py
+++ helpers.py
@@ -18,12 +18,18 @@ def change_board_event(board, coordinate, new_event, description):
     board[coordinate]['Event'] = new_event
     board[coordinate]['Description'] = description
 
+
 def out_of_bounds(entity, board, direction):
     direction_key = {'n': 1, 's': -1, 'e': -1, 'w': 1}
     board_row = list(board.keys())[-1][0]
-    board_height = list(board.keys())[-1][1]
     board_col = list(board.keys())[-1][2]
 
     if direction == 'n' or direction == 's':
         if entity['Z'] + direction_key[direction] > board_col or entity['Z'] + direction_key[direction] < 0:
             return True
+
+    if direction == 'e' or direction == 'w':
+        if entity['X'] + direction_key[direction] > board_row or entity['X'] + direction_key[direction] < 0:
+            return True
+
+    return False
