affcc7b28e9e15d86629ee83a32372ed735f08bf
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-24 18:33:58 -0800

    factor in entity spd

--- helpers.py
+++ helpers.py
@@ -21,23 +21,33 @@ def change_board_event(board, coordinate, new_event, description):
 
 def out_of_bounds(entity, board, direction):
     direction_key = {'n': 1, 's': -1, 'e': -1, 'w': 1, 'up': 1, 'down': -1}
+    entity_spd = entity['SPD']
     board_row = list(board.keys())[-1][0]
     board_col = list(board.keys())[-1][2]
     board_height = list(board.keys())[-1][1]
 
     if direction == 'n' or direction == 's':
-        new_position = entity['Z'] + direction_key[direction]
+        new_position = entity['Z'] + direction_key[direction] * entity_spd
         if new_position > board_col or new_position < 0:
             return True
 
     if direction == 'e' or direction == 'w':
-        new_position = entity['X'] + direction_key[direction]
+        new_position = entity['X'] + direction_key[direction] * entity_spd
         if new_position > board_row or new_position < 0:
             return True
 
     if direction == 'up' or direction == 'down':
-        new_position = entity['Y'] + direction_key[direction]
+        new_position = entity['Y'] + direction_key[direction] * entity_spd
         if new_position > board_height or new_position < 0:
             return True
 
     return False
+
+
+def move(entity, direction):
+    entity_spd = entity['SPD']
+    direction_key = {'n': 1, 's': -1, 'e': -1, 'w': 1, 'up': 1, 'down': -1}
+
+    entity['X'] += entity_spd * direction_key[direction] if direction == 'e' or direction == 'w' else 0
+    entity['Z'] += entity_spd * direction_key[direction] if direction == 'n' or direction == 's' else 0
+    entity['Y'] += entity_spd * direction_key[direction] if direction == 'up' or direction == 'down' else 0
