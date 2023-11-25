36423b4e94d347d8de1f941bcb18a6fd083c3048
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-24 17:00:22 -0800

    switch to match case

--- main.py
+++ main.py
@@ -27,24 +27,20 @@ def run_game():
         # process input
         while True:
             player_input = helpers.enforced_input('Input: ', VALID_ACTIONS)
-            if player_input == 'help':
-                process_help()
-
-            if player_input == 'take':
-                if process_take(player, building):
-                    break
-
-            elif player_input == 'move':
-                process_move(player, building)
-                break
-
-            elif player_input == 'listen':
-                process_listen(player, monster)
-                break
-
-            elif player_input == 'flash':
-                process_flash(player, monster)
-                break
+            match player_input:
+                case 'help':
+                    process_help()
+                    continue
+                case 'take':
+                    if not process_take(player, building):
+                        continue
+                case 'move':
+                    process_move(player, building)
+                case 'listen':
+                    process_listen(player, monster)
+                case 'flash':
+                    process_flash(player, monster)
+            break
 
         print(player['X'], player['Y'], player['Z'])
         describe_location(player, building)
