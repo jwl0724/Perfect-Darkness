fe546fae01d64aad37294ee8ff81b2a44e7e966f
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-24 16:51:14 -0800

    add constants and break statements

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def run_game():
     player_coords, monster_coords = (2, 0, 0), (2, 2, 4)
     rows, height, column = 5, 3, 5
 
+    # set constants
+    VALID_ACTIONS = ('help', 'take', 'move', 'listen', 'flash')
+
     # intialize the game
     introduce_game()
     player = create_entity(player_stats, player_coords, 1, True)
@@ -23,7 +26,7 @@ def run_game():
     while is_alive(player):
         # process input
         while True:
-            player_input = get_action_input()
+            player_input = helpers.enforced_input('Input: ', VALID_ACTIONS)
             if player_input == 'help':
                 process_help()
 
@@ -33,12 +36,15 @@ def run_game():
 
             elif player_input == 'move':
                 process_move(player, building)
+                break
 
             elif player_input == 'listen':
                 process_listen(player, monster)
+                break
 
             elif player_input == 'flash':
                 process_flash(player, monster)
+                break
 
         print(player['X'], player['Y'], player['Z'])
         describe_location(player, building)
