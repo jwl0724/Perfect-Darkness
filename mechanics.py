34c4ec5283cb17ce730602d794b85caa079fdce7
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-24 16:30:25 -0800

    move help process into its own function

--- mechanics.py
+++ mechanics.py
@@ -21,15 +21,17 @@ def get_action_input():
     # process action
     while True:
         player_input = helpers.enforced_input('Input: ', valid_action)
-        if player_input == 'help':
-            print("""
-            Take - Spend a turn to take an item, with a 5% chance of failing
-            Move - Spend a turn to move in a direction
-            Listen - Spend a turn to focus listening to estimate where the monster is
-            Flash - Spend a turn to get exact location of the monster, also alerts the monster to your location
-            """)
-        elif player_input in valid_action:
+        if player_input in valid_action:
             return player_input
+
+
+def process_help():
+    print("""
+                Take - Spend a turn to take an item, with a 5% chance of failing
+                Move - Spend a turn to move in a direction
+                Listen - Spend a turn to focus listening to estimate where the monster is
+                Flash - Spend a turn to get exact location of the monster, also alerts the monster to your location
+          """)
 
 
 def process_take(player, board):
