421002b30ede41fd2a9c4bc580566fe819753106
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-26 02:13:02 -0800

    finish chase player

--- monster.py
+++ monster.py
@@ -17,4 +17,24 @@ def move_monster(monster, board):
 
 
 def chase_player(player, monster):
-    pass
+    if player['X'] != monster['X']:
+        direction = 'e' if player['X'] > monster['X'] else 'w'
+        difference = player['X'] - monster['X']
+        move_unit = monster['SPD'] if difference > 2 else difference
+        helpers.move(monster, direction, move_unit)
+
+    elif player['Y'] != monster['Y']:
+        direction = 'up' if player['Y'] > monster['Y'] else 'down'
+        difference = player['Y'] - monster['Y']
+        move_unit = monster['SPD'] if difference > 2 else difference
+        helpers.move(monster, direction, move_unit)
+
+    elif player['Z'] != monster['Z']:
+        direction = 'n' if player['Z'] > monster['Z'] else 's'
+        difference = player['Z'] - monster['Z']
+        move_unit = monster['SPD'] if difference > 2 else difference
+        helpers.move(monster, direction, move_unit)
+
+    monster['Alert Counter'] -= 1
+    if monster['Alert Counter'] == 0:
+        monster['Alerted'] = False
