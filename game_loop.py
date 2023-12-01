9c81bd62355dcd13d2db2b3603efe3ae474ebd00
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-30 18:13:43 -0800

    create new file to integrate pygame to game

--- game_loop.py
+++ game_loop.py
@@ -6,7 +6,8 @@ import monster as mon
 import helpers
 import input as ipt
 import pygame as pg
-
+import pygame_helpers as pg_help
+import descriptions as desc
 
 def run_game():
     # set initial variables
@@ -14,6 +15,20 @@ def run_game():
     player_coords, monster_coords = (2, 0, 0), (2, 2, 4)
     rows, height, column = 5, 3, 5
     game_running = True
+
+    # set file image and audio file paths
+    knife_img, armor_img, bottle_img = 'Images/knife.jpg', 'Images/armor.jpg', 'Images/bottle.jpg'
+    gun_img, lid_img, scissor_img = 'Images/gun.jpg', 'Images/lid.jpg', 'Images/scissor.jpg'
+    monster_img, default_img, flash_img = 'Images/monster.jpg', 'Images/default.jpg', 'Images/flash.jpg'
+
+    intro_msg = """
+    Welcome to Perfect Darkness, you are a captured convict sent into a building under sanction. You are tasked 
+    with eliminating the creature confined within the building. The foundation has not provided any tools or weapons 
+    to help in your task, armed with nothing but your wits and flashlight, you must scour through the building to find 
+    any semblance of equipment to help you in your task. But be careful, the building is in complete darkness with no 
+    hopes of seeing the creature. You must rely on the subtle sounds the creature makes as it roams around, 
+    looking for prey within it's domain. If at any point you wish to bring up a list of commands, type help.
+    """
 
     # set constants
     VALID_ACTIONS = ('help', 'take', 'move', 'listen', 'flash', 'save', 'load', 'delete')
@@ -23,11 +38,14 @@ def run_game():
     monster = init.create_entity(monster_stats, monster_coords, 2, False)
     building = init.make_board(rows, height, column)
 
-    # initialize pygame screen
+    # initialize pygame resources
+    pg.init()
     screen = pg.display.set_mode((1080, 720))
     pg.display.set_caption('Perfect Darkness')
-    screen.fill((255, 255, 255))
-
+    screen.fill((0, 0, 0))
+    pg_help.draw_windows(screen)
+    pg_help.draw_image(screen, monster_img)
+    pg_help.draw_text(screen, desc.intro, 60)
     pg.display.flip()
 
     while game_running:
@@ -36,6 +54,7 @@ def run_game():
             game_running = False
         if event.type == pygame.KEYDOWN:
             pass
+
     pg.quit()
 
 
