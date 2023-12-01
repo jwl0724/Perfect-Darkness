3d13ff25dede49dc5750b8aebd65fad08151550f
Jonathan Liu <jwliu0724@hotmail.com>
2023-11-30 18:13:01 -0800

    move descriptions into own file

---
+++ descriptions.py
@@ -0,0 +1,14 @@
+intro = (
+    'Welcome to Perfect Darkness',
+    'You are a capture convict',
+    'You are sent into a building under sanction',
+    'You are tasked with eliminate a creature',
+    'The foundation has not provided any assistance',
+    'Armed with nothing but a flashlight',
+    'You must scour through the building for equipment',
+    'But be careful, the building is completely dark',
+    'You have no hopes of seeing the creature',
+    'You must rely on subtle sounds cues',
+    'While the monster hunts for prey',
+    'Try not to die...'
+)
