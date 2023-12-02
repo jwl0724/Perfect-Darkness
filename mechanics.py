be01c72bbde75de70b101242d4b44fb3fdbc60d8
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-12-02 04:15:55 -0800

    integrate pygame into fight

--- mechanics.py
+++ mechanics.py
@@ -1,69 +1,67 @@
 import random
 import math
 import helpers
-
+import pygame as pg
+import pygame_helpers as pg_help
+import constants as con
 
 def describe_location(player, board):
     description = board[(player['X'], player['Y'], player['Z'])]['Description']
     return description
 
 
-def fight(player, monster):
+def fight(screen, valid_inputs, player, monster):
     fighting = True
-    valid_actions = ('attack', 'mend', 'flee', 'help', 'status')
+    pg_help.draw_one_line_text(screen, 'The creature stares at you with it\'s unblinking eyes...')
 
     while fighting:
-        print('The creature stares at you with it\'s unblinking eyes...')
-
-        action = helpers.enforced_input('Action: ', valid_actions)
-        match action:
-            case 'help':
-                print("""
-                Attack - Spend a turn to attack the enemy, with a 10% chance to miss
-                Mend - Spend a turn to heal 80% of your missing HP
-                Flee - Spend a turn to try and run away from the enemy, with a 25% chance to fail
-                Status - See your current health and the enemy's current condition
-                """)
-                continue
-
-            case 'status':
-                print(f'HP: {player["HP"]}/{player["MAX HP"]}, ATK: {player["ATK"]}, DEF: {player["DEF"]}')
+        pg_help.draw_multi_line_text(screen, con.fight_help_msg_list)
+        key_pressed = pg_help.wait_for_input(valid_inputs)
+        match key_pressed:
+            case pg.K_3:
+                player_stats = f'HP: {player["HP"]}/{player["MAX HP"]}, ATK: {player["ATK"]}, DEF: {player["DEF"]}'
+                
                 mon_hp = monster['HP']
-
                 match mon_hp:
                     case mon_hp if helpers.is_between(mon_hp, monster['MAX HP'] * 0.8, monster['MAX HP']):
-                        print('The creature appears to has taken minimal damage...')
+                        monster_condition = 'The creature appears to has taken minimal damage...'
                     case mon_hp if helpers.is_between(mon_hp, monster['MAX HP'] * 0.6, monster['MAX HP'] * 0.8):
-                        print('You can hear ragged breathing emanating from the creature, it seems tired...')
+                        monster_condition = 'You can hear ragged breathing emanating from the creature, it seems tired...'
                     case mon_hp if helpers.is_between(mon_hp, monster['MAX HP'] * 0.4, monster['MAX HP'] * 0.6):
-                        print('The hear banshee like screeches from the creature, it must be halfway dead...')
+                        monster_condition = 'The hear banshee like screeches from the creature, it must be halfway dead...'
                     case mon_hp if helpers.is_between(mon_hp, monster['MAX HP'] * 0.2, monster['MAX HP'] * 0.4):
-                        print('The smell of rotting flesh and blood permeate from the creature, almost there...')
+                        monster_condition = 'The smell of rotting flesh and blood permeate from the creature, almost there...'
                     case mon_hp if helpers.is_between(mon_hp, 0, monster['MAX HP'] * 0.2):
-                        print('Your can hear blood dripping in the darkness, the creature must be near death..')
+                        monster_condition = 'Your can hear blood dripping in the darkness, the creature must be near death..'
+                pg_help.draw_multi_line_text(screen, [player_stats, monster_condition])
                 continue
 
-            case 'attack':
+            case pg.K_1:
                 evade_msg = 'You lunged forward at the creature, but creature used the darkness to evade your attack.'
                 hit_msg = 'You lunged forward at the creature and feel flesh tearing as you swing through the darkness.'
                 if random.randint(1, 10) == 1:
-                    print(evade_msg)
+                    pg_help.draw_one_line_text(screen, evade_msg)
                 else:
                     monster['HP'] -= math.ceil(player['ATK'] * (1 - monster['DEF'] / 100))
-                    print(hit_msg)
+                    pg_help.draw_one_line_text(screen, hit_msg)
 
-            case 'mend':
+            case pg.K_2:
                 heal_amount = math.floor((player['MAX HP'] - player['HP']) * 0.8)
                 player['HP'] += 1 if player['HP'] == 9 else heal_amount
                 if player['HP'] == player['MAX HP']:
-                    print('You try to abuse your adrenaline rush, but you\'re already at max capacity.')
-                print('Your abuse your adrenaline rush, allowing you to take more hits from the creature.')
+                    fail_heal_msg = 'You try to abuse your adrenaline rush, but you failed'
+                    pg_help.draw_one_line_text(screen, fail_heal_msg)
+                else:
+                    heal_msg = 'Your abuse your adrenaline rush, allowing you to take more hits from the creature.'
+                    pg_help.draw_one_line_text(screen, heal_msg)
 
-            case 'flee':
+            case pg.K_4:
                 if random.randint(1, 4) != 1:
-                    print('You managed to evade the creature temporarily and live another day.')
+                    esc_msg = 'You managed to evade the creature temporarily'
+                    pg_help.draw_one_line_text(screen, esc_msg)
                     return
-                print('You attempt to run, but the creature shrieks before you can even move, paralyzing you in fear.')
+                fail_esc_msg_list = ['You attempted to run', 'But the creature shrieks before you can move', 'You are paralyzed in fear']
+                pg_help.draw_one_line_text(screen, fail_esc_msg_list)
 
         monster_actions = ('attack', 'heal', 'nothing')  # 75%, 20%, 5% respectively
         random_num = random.randint(1, 100)
@@ -78,18 +76,29 @@ def fight(player, monster):
         match action:
             case 'attack':
                 if random.randint(1, 10) == 1:
-                    print('The creature slashes from the darkness, but you just barely managed to avoid the hit.')
+                    miss_msg_list = ['The creature slashes from the darkness', 'You just barely avoided the hit']
+                    pg_help.draw_one_line_text(screen, miss_msg_list)
                 else:
                     player['HP'] -= math.ceil(monster['ATK'] * (1 - player['DEF'] / 100))
-                    print('The creature ambushes you from the darkness and takes a chunk of flesh from your body.')
+                    hit_msg_list = ['The creature ambushes you from the darkness', 'It takes a chunk of flesh from your body']
+                    pg_help.draw_one_line_text(hit_msg_list)
 
             case 'heal':
                 monster['HP'] += random.randint(1, 5)
                 if monster['HP'] > monster['MAX HP']:
                     monster['HP'] = 50
-                print('The creature rushes into the darkness and consumes something, it must have healed...')
+                monster_heal_msg_list = [
+                    'The creature rushes into the darkness', 
+                    'You hear the sound of flesh being chewed',
+                    'It must have healed...'
+                ]
+                pg_help.draw_one_line_text(screen, monster_heal_msg_list)
 
             case 'nothing':
-                print('The creature observes you from a distance, before letting out an ear piercing screech.')
+                monster_nothing_msg_list = [
+                    'The creature observes you from a distance',
+                    'It\'s cautiously analyzing you...',
+                ]
+                pg_help.draw_one_line_text(screen, monster_nothing_msg_list)
 
         fighting = True if helpers.is_alive(player) and helpers.is_alive(monster) else False
