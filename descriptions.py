2b543d0b569528032314f476420f2ceeee68526b
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-12-01 22:15:53 -0800

    add messages for help

--- descriptions.py
+++ descriptions.py
@@ -1,8 +1,8 @@
-intro = (
+intro_msg_list = (
     'Welcome to Perfect Darkness',
     'You are a capture convict',
     'You are sent into a building under sanction',
-    'You are tasked with eliminate a creature',
+    'You are tasked with eliminating a creature',
     'The foundation has not provided any assistance',
     'Armed with nothing but a flashlight',
     'You must scour through the building for equipment',
@@ -11,4 +11,14 @@ intro = (
     'You must rely on subtle sounds cues',
     'While the monster hunts for prey',
     'Try not to die...'
+)
+
+overworld_help_msg_list = (
+    'Take - Spend a turn to take an item, with a 5% chance of failing',
+    'Move - Spend a turn to move in a direction',
+    'Listen - Spend a turn to focus listening to estimate where the monster is',
+    'Flash - Spend a turn to get your surroundings, also alerts the monster if it\'s on the same floor',
+    'Save - Save the current state of the game',
+    'Load - Load a previous save file',
+    'Delete - Delete a previous save file'
 )
