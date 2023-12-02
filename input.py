fb70babc250bc25b2ed4e4e247f88d2070fa2139
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-12-02 02:43:06 -0800

    repalce print statements with draw text on window

--- input.py
+++ input.py
@@ -2,21 +2,12 @@ import random
 import helpers
 import json
 import os
+import constants as con
+import pygame as pg
+import pygame_helpers as pg_help
 
 
-def process_help():
-    print("""
-                Take - Spend a turn to take an item, with a 5% chance of failing
-                Move - Spend a turn to move in a direction
-                Listen - Spend a turn to focus listening to estimate where the monster is
-                Flash - Spend a turn to get your surroundings, also alerts the monster if it's on the same floor
-                Save - Save the current state of the game
-                Load - Load a previous save file
-                Delete - Delete a previous save file
-          """)
-
-
-def process_take(player, board):
+def process_take(screen, player, board):
     """
     Process taking items from board
 
@@ -30,13 +21,14 @@ def process_take(player, board):
     player_coords = (player['X'], player['Y'], player['Z'])
     board_event = board[player_coords]['Event']
     if board_event != 'Salvage':
-        print('There\'s nothing to take.')
+        pg_help.draw_one_line_text(screen, 'There\'s nothing to take.', wait=False)
         return False
 
     random_number = random.randint(1, 20)
     if random_number == 4:
-        print('You tried to grab the item, but fumbled in the darkness. The item is now lost into void of darkness.')
-        new_description = 'There\'s nothing here but regret, since you fumbled grabbing the item, it\'s now lost in the darkness'
+        fail_msg = 'You tried to grab the item, but fumbled in the darkness.'
+        pg_help.draw_one_line_text(screen, fail_msg)
+        new_description = 'There\'s nothing here but regret, since you fumbled grabbing the item'
         helpers.change_board_event(board, player_coords, 'None', new_description)
         return True
 
@@ -47,57 +39,55 @@ def process_take(player, board):
 
     for item in all_items:
         if item in board[player_coords]['Description']:
-            print(f'You managed to grab the {item} without issue.')
+            pg_help.draw_one_line_text(screen, f'You managed to grab the {item} without issue.')
             player['ATK'] += 5 if item in atk_booster else 0
             player['DEF'] += 15 if item in def_booster else 0
 
             feedback_message = f'You now have increased offensive power with the {item}.' if item in atk_booster else\
                 f'You now take less damage thanks to your {item}'
-            print(feedback_message)
+            pg_help.draw_one_line_text(screen, feedback_message)
             new_description = 'An empty void of darkness, it used to contain things, but you looted it.'
             helpers.change_board_event(board, player_coords, 'None', new_description)
 
     return True
 
 
-def process_move(player, monster, board):
-    valid_direction = ('n', 's', 'e', 'w', 'up', 'down', 'help')
-    player_not_moved = True
+def process_move(screen, player, monster, board, key_pressed):
+    if key_pressed == pg.K_UP and board[(player['X'], player['Y'], player['Z'])]['Event'] != 'Stairs':
+        pg_help.draw_one_line_text(screen, 'There\'s nothing to climb.', wait=False)
+        return False
 
-    while player_not_moved:
-        direction = helpers.enforced_input('Which direction would you like to move? (N, S, E, W): ', valid_direction)
+    elif key_pressed == pg.K_DOWN and board[(player['X'], player['Y'], player['Z'])]['Event'] != 'Hole':
+        pg_help.draw_one_line_text(screen, 'There\'s no hole to jump into.', wait=False)
+        return False
 
-        if direction == 'help':
-            print('Valid directions include: N, S, E, W, Up, Down')
-            continue
-        elif direction == 'up' and board[(player['X'], player['Y'], player['Z'])]['Event'] != 'Stairs':
-            print('There\'s nothing to climb up from.')
-            continue
-        elif direction == 'down' and board[(player['X'], player['Y'], player['Z'])]['Event'] != 'Hole':
-            print('There\'s no hole to jump into.')
-            continue
-        elif helpers.out_of_bounds(player, board, direction):
-            print('You moved into a wall... Try again.')
-            continue
+    elif helpers.out_of_bounds(player, board, key_pressed):
+        pg_help.draw_one_line_text(screen, 'You bumped into a wall.', wait=False)
+        return False
 
-        if monster['Alerted']:
-            trip_message = 'In your panic you tripped over some debris hidden in the darkness, you failed to move.'
-            helpers.move(player, direction) if random.randint(1, 5) != 1 else print(trip_message)
-        else:
-            helpers.move(player, direction)
+    if monster['Alerted']:
+        trip_message = 'In your panic you tripped over some debris hidden in the darkness, you failed to move.'
+        helpers.move(player, key_pressed) if random.randint(1, 5) != 1 else pg_help.draw_one_line_text(screen, trip_message)
+    else:
+        helpers.move(player, key_pressed)
 
-        player_not_moved = False
+    return True
 
+def process_flash(screen, player, monster, board):
+    pg_help.draw_image(screen, con.flash_img)
+    pg_help.draw_one_line_text(screen, 'You briefly shine your flashlight...')
 
-def process_flash(player, monster):
-    print('You briefly flash your flashlight...')
     if monster['Y'] != player['Y']:
-        print('You see no sign of the creature, it must be on a different floor.')
+        pg_help.draw_one_line_text(screen, 'You see no sign of the creature, it must be on a different floor.')
         return
 
     monster['Alerted'] = True
     monster['Alert Counter'] = 5
-    print('You hear rabid snarling coming from the darkness, it seems you have alerted it to your presence.')
+    
+    # pg_help.play_sound(con.alert_sound)
+    alert_msg = 'You hear rabid snarling coming from the darkness, it seems you have alerted it to your presence.'
+    pg_help.draw_one_line_text(screen, alert_msg)
+    pg_help.draw_map(screen, player, monster, board)
 
     # pending to be removed, will print out a map with monster on the board in tkinter later
     # x_difference = monster['X'] - player['X']
@@ -118,15 +108,15 @@ def process_flash(player, monster):
     # print(warning_message)
 
 
-def process_listen(player, monster):
-    print('You stop everything, and focus intensely on listening to your surroundings.')
+def process_listen(screen, player, monster):
+    pg_help.draw_one_line_text(screen, 'You stop everything and focus intensely on listening to your surroundings.')
 
     # process monster on different floor as player
     if player['Y'] > monster['Y']:
-        print('You pick up on some subtle shuffling on the floors below, the monster must be there...')
+        pg_help.draw_one_line_text(screen, 'You pick up on some subtle shuffling on the floors below, the monster must be there...')
         return
     elif player['Y'] < monster['Y']:
-        print('You pick up some faint movement on the floors above, the monster must be there...')
+        pg_help.draw_one_line_text(screen, 'You pick up some faint movement on the floors above, the monster must be there...')
         return
 
     monster_location_description = 'You pick up some faint sounds coming from '
@@ -142,7 +132,7 @@ def process_listen(player, monster):
 
     monster_location_description += ' of you. Best to avoid that place for now...'
 
-    print(monster_location_description)
+    pg_help.draw_one_line_text(screen, monster_location_description)
 
 
 def process_save(player, monster, board):
