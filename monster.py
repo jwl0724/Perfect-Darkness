6097317125612ca11e537c5d729710e1ee5c6df5
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-12-02 02:43:23 -0800

    replace monster direction with keypresses

--- monster.py
+++ monster.py
@@ -1,14 +1,14 @@
 import random
 import helpers
-
+import pygame as pg
 
 def move_monster(monster, board):
-    available_directions = ('n', 's', 'e', 'w', 'up', 'down')
+    available_keys = (pg.K_w, pg.K_s, pg.K_d, pg.K_a, pg.K_UP, pg.K_DOWN)
     monster_not_moved = True
 
     while monster_not_moved:
         move_unit = random.randint(1, monster['SPD'])
-        direction = available_directions[random.randint(0, 5)]
+        direction = available_keys[random.randint(0, 5)]
         if helpers.out_of_bounds(monster, board, direction, move_unit):
             continue
 
@@ -18,19 +18,19 @@ def move_monster(monster, board):
 
 def chase_player(player, monster):
     if player['X'] != monster['X']:
-        direction = 'e' if player['X'] > monster['X'] else 'w'
+        direction = pg.K_d if player['X'] > monster['X'] else pg.K_a
         difference = abs(player['X'] - monster['X'])
         move_unit = monster['SPD'] if difference > 2 else difference
         helpers.move(monster, direction, move_unit)
 
     elif player['Y'] != monster['Y']:
-        direction = 'up' if player['Y'] > monster['Y'] else 'down'
+        direction = pg.K_UP if player['Y'] > monster['Y'] else pg.K_DOWN
         difference = abs(player['Y'] - monster['Y'])
         move_unit = monster['SPD'] if difference > 2 else difference
         helpers.move(monster, direction, move_unit)
 
     elif player['Z'] != monster['Z']:
-        direction = 'n' if player['Z'] > monster['Z'] else 's'
+        direction = pg.K_w if player['Z'] > monster['Z'] else pg.K_s
         difference = abs(player['Z'] - monster['Z'])
         move_unit = monster['SPD'] if difference > 2 else difference
         helpers.move(monster, direction, move_unit)
