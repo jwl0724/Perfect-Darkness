7d4b0c5008c42d3d867b713315bb966586309755
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-12-01 21:47:42 -0800

    add helpers for sound, draw text, and inputs

--- pygame_helpers.py
+++ pygame_helpers.py
@@ -14,17 +14,16 @@ def draw_image(screen, image_path):
     pg.display.flip()
 
 
-def wait_for_story():
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-            if event.type == pg.KEYDOWN and event.key == pg.K_RETURN:
-                return
-
-
-def draw_story_text(screen, msg_list, size):
+def draw_one_line_text(screen, msg_list, size=60):
+    if type(msg_list) == str:
+        pg.draw.rect(screen, (0, 0, 0), pg.Rect(25, 520, 1030, 180))
+        text = font.render(msg_list, True, (255, 255, 255))
+        text_rect = text.get_rect()
+        text_rect.center = (520, 605)
+        screen.blit(text, text_rect)
+        pg.display.flip()
+        return
+    
     font = pg.font.Font('Fonts/Amatic-Bold.ttf', size)
 
     for msg in msg_list:
@@ -36,12 +35,22 @@ def draw_story_text(screen, msg_list, size):
         pg.display.flip()
 
         # make user press enter to continue
-        wait_for_story()
+        wait_for_input([pg.K_RETURN])
 
     pg.draw.rect(screen, (0, 0, 0), pg.Rect(25, 520, 1030, 180))
     pg.display.flip()
     
 
+def play_sound(file_path):
+    print(file_path)
+    pg.mixer.music.load(file_path)
+    pg.mixer.music.play()
 
-def wait_for_overworld():
-    while True
+
+def wait_for_input(valid_inputs):
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+            elif event.type == pg.KEYDOWN and event.key in valid_inputs:
+                return event.key
