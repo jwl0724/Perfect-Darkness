3962078a4b316b9f2ec618589d2dcc4f1484043c
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-12-01 20:37:46 -0800

    finish introduction in pygame

--- pygame_helpers.py
+++ pygame_helpers.py
@@ -11,9 +11,20 @@ def draw_image(screen, image_path):
     img = pg.image.load(image_path).convert()
     transformed_img = pg.transform.scale(img, (1030, 470))
     screen.blit(transformed_img, (25, 25))
+    pg.display.flip()
 
 
-def draw_text(screen, msg_list, size):
+def wait_for_story():
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.KEYDOWN and event.key == pg.K_RETURN:
+                return
+
+
+def draw_story_text(screen, msg_list, size):
     font = pg.font.Font('Fonts/Amatic-Bold.ttf', size)
 
     for msg in msg_list:
@@ -21,18 +32,16 @@ def draw_text(screen, msg_list, size):
         text = font.render(msg, True, (255, 255, 255))
         text_rect = text.get_rect()
         text_rect.center = (520, 605)
-        press_enter_msg = font.render('Press Enter to Continue', True, (255, 255, 255))
-        enter_msg_rect = press_enter_msg.get_rect()
-        enter_msg_rect.center = (520, 610 + size)
+        screen.blit(text, text_rect)
+        pg.display.flip()
 
         # make user press enter to continue
-        while True:
-            event = pg.event.wait()
-            if event == pg.QUIT:
-                pg.display.quit()
-                pg.quit()
-                sys.exit()
+        wait_for_story()
 
-            if event == pg.KEYDOWN:
-                if event.key == pg.K_RETURN:
-                    break
+    pg.draw.rect(screen, (0, 0, 0), pg.Rect(25, 520, 1030, 180))
+    pg.display.flip()
+    
+
+
+def wait_for_overworld():
+    while True
