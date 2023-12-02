f5d30dbee64457ba428010edd322c1e45ddd12d9
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2023-12-01 22:15:38 -0800

    add drawing multiple lines

--- pygame_helpers.py
+++ pygame_helpers.py
@@ -14,24 +14,38 @@ def draw_image(screen, image_path):
     pg.display.flip()
 
 
-def draw_one_line_text(screen, msg_list, size=60):
-    if type(msg_list) == str:
-        pg.draw.rect(screen, (0, 0, 0), pg.Rect(25, 520, 1030, 180))
-        text = font.render(msg_list, True, (255, 255, 255))
-        text_rect = text.get_rect()
-        text_rect.center = (520, 605)
-        screen.blit(text, text_rect)
-        pg.display.flip()
-        return
-    
+def draw_one_line_text(screen, msg_list, size=60, wait=False):
+    # initialize font-style
     font = pg.font.Font('Fonts/Amatic-Bold.ttf', size)
 
-    for msg in msg_list:
+    if type(msg_list) == str:
+        # draw console background to replace previous text
         pg.draw.rect(screen, (0, 0, 0), pg.Rect(25, 520, 1030, 180))
-        text = font.render(msg, True, (255, 255, 255))
+
+        # render text
+        text = font.render(msg_list, True, (255, 255, 255))
         text_rect = text.get_rect()
+
+        # adjust text location and draw it
         text_rect.center = (520, 605)
         screen.blit(text, text_rect)
+
+        # update screen
+        pg.display.flip()
+
+        # wait for enter only if specified in call
+        wait_for_input([pg.K_RETURN]) if wait == True else None
+        return
+    
+    for msg in msg_list:
+        pg.draw.rect(screen, (0, 0, 0), pg.Rect(25, 520, 1030, 180))
+
+        text = font.render(msg, True, (255, 255, 255))
+        text_rect = text.get_rect()
+
+        text_rect.center = (520, 605)
+        screen.blit(text, text_rect)
+
         pg.display.flip()
 
         # make user press enter to continue
@@ -40,6 +54,20 @@ def draw_one_line_text(screen, msg_list, size=60):
     pg.draw.rect(screen, (0, 0, 0), pg.Rect(25, 520, 1030, 180))
     pg.display.flip()
     
+
+def draw_multi_line_text(screen, msg_list, size=40):
+    font = pg.font.Font('Fonts/Amatic-Bold.ttf', size)
+    pg.draw.rect(screen, (0, 0, 0), pg.Rect(25, 520, 1030, 180))
+
+    for index, msg in enumerate(msg_list):
+        text = font.render(msg, True, (255, 255, 255))
+        text_rect = text.get_rect()
+
+        text_rect.center = (520, 605 + index * size)
+        screen.blit(text, text_rect)
+
+        pg.display.flip()
+
 
 def play_sound(file_path):
     print(file_path)
