41052615dcaa8daead69adc378d403ed0e291c49
Jonathan Liu <jwliu0724@hotmail.com>
2023-12-04 14:04:04 -0800

    finish docstrings and add a doctest to chase_player

--- monster.py
+++ monster.py
@@ -3,7 +3,18 @@ import helpers
 import pygame as pg
 
 
-def move_monster(monster, board):
+def move_monster(monster: dict, board: dict) -> None:
+    """
+    Move the monster
+
+    This function randomly generates a direction and movement unit for the monster to move towards, and if it results in
+    the monster going out of bounds, a new direction and unit is generated until it stays in bound
+
+    :param monster: A dictionary that represents a monster
+    :param board: A dictionary that represents the playable area
+    :precondition: monster and board must accurately reflect what they represent and is correctly formatted
+    :postcondition: The monster coordinates are changed
+    """
     available_keys = (pg.K_w, pg.K_s, pg.K_d, pg.K_a, pg.K_UP, pg.K_DOWN)
     monster_not_moved = True
 
@@ -17,7 +28,29 @@ def move_monster(monster, board):
         monster_not_moved = False
 
 
-def chase_player(player, monster):
+def chase_player(player: dict, monster: dict) -> None:
+    """
+    Move monster towards player
+
+    This function will move the monster towards the coordinates of which the player resides at when called
+
+    :param player: A dictionary that represents the player that has the player's coordinates
+    :param monster: A dictionary that represents the monster that has the monster's coordinates
+    :precondition: player and monster must be dictionaries that represent the player and monster respectively
+    :postcondition: The monster coordinates will be moved towards the player's coordinates until they overlap
+
+    >>> sample_player = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 1}
+    >>> sample_monster = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 2, 'Y': 0, 'Z': 0, 'SPD': 2, 'Alerted': True, 'Alert Counter': 20}
+    >>> chase_player(sample_player, sample_monster)
+    >>> sample_monster
+    {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 2, 'Alerted': True, 'Alert Counter': 19}
+
+    >>> sample_player = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 1}
+    >>> sample_monster = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 1, 'SPD': 2, 'Alerted': True, 'Alert Counter': 1}
+    >>> chase_player(sample_player, sample_monster)
+    >>> sample_monster
+    {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 2, 'Alerted': False, 'Alert Counter': 0}
+    """
     if player['X'] != monster['X']:
         direction = pg.K_d if player['X'] > monster['X'] else pg.K_a
         difference = abs(player['X'] - monster['X'])
