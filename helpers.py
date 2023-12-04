887d5dbd5ee010f874c96c260440c924d8ff12d3
Jonathan Liu <jwliu0724@hotmail.com>
2023-12-03 21:03:10 -0800

    finish docstring and doctests for all functions

--- helpers.py
+++ helpers.py
@@ -73,7 +73,7 @@ def out_of_bounds(entity: dict, board: dict, direction: pg.constants, speed: int
     :param speed: An integer representing amount of units to move, defaults to None when no argument is passed
     :precondition: entity and board must be dictionaries representing an entity and the playable area respectively
     :precondition: direction must be a pygame constant for the keys W, A, S, D, up arrow, down arrow
-    :postcondition: The validity of the movement is evaluated and returned
+    :postcondition: The validity of the direction of movement is evaluated
     :return: A boolean True if the input direction goes out of bounds, False otherwise
 
     >>> test_board = {(0, 0, 0): {'Event': 'None', 'Description':  'Wow'}, (1, 0, 0): {'Event': 'None', 'Description': 'Cool'}}
@@ -116,28 +116,118 @@ def out_of_bounds(entity: dict, board: dict, direction: pg.constants, speed: int
     return False
 
 
-def move(entity, key_pressed, speed=None):
+def move(entity: dict, key_pressed: pg.constants, speed=None) -> None:
+    """
+    Move an entity in a direction
+
+    This function moves an entity's coordinates in a direction by a unit of the entity's speed if no speed is
+    specified, otherwise move the parameter speed amount of units
+
+    :param entity: A dictionary that represents an entity in the game (player or monster)
+    :param key_pressed: A pygame constant that represents a keyboard button that was pressed
+    :param speed: An integer representing the amount of units to move, defaults to None when no argument passed
+    :precondition: entity must be a dictionary representing an entity (player or monster)
+    :precondition: key_pressed must be a pygame constant that represents a keyboard button that was pressed
+    :precondition: key_pressed must in a valid direction that stays within the board's bounds
+    :postcondition: The entity will now have a different coordinate based on the direction it moved
+
+    >>> test_board = {(0, 0, 0): {'Event': 'None', 'Description':  'Wow'}, (1, 0, 0): {'Event': 'None', 'Description': 'Cool'}}
+    >>> sample_player = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 1}
+    >>> move(sample_player, pg.K_d)
+    >>> sample_player
+    {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 1, 'Y': 0, 'Z': 0, 'SPD': 1}
+
+    >>> test_board = {(0, 0, 0): {'Event': 'Stairs', 'Description':  'Wow'}, (0, 1, 0): {'Event': 'None', 'Description': 'Cool'}}
+    >>> sample_player = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 1}
+    >>> move(sample_player, pg.K_UP)
+    >>> sample_player
+    {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 1, 'Z': 0, 'SPD': 1}
+    """
+    # set speed to the parameter speed, if one was passed
     entity_spd = speed if speed else entity['SPD']
     direction_key = {pg.K_w: 1, pg.K_s: -1, pg.K_d: 1, pg.K_a: -1, pg.K_UP: 1, pg.K_DOWN: -1}
 
+    # move the entity
     entity['X'] += entity_spd * direction_key[key_pressed] if key_pressed == pg.K_a or key_pressed == pg.K_d else 0
     entity['Z'] += entity_spd * direction_key[key_pressed] if key_pressed == pg.K_s or key_pressed == pg.K_w else 0
     entity['Y'] += entity_spd * direction_key[key_pressed] if key_pressed == pg.K_UP or key_pressed == pg.K_DOWN else 0
 
 
-def is_alive(entity):
+def is_alive(entity: dict) -> bool:
+    """
+    Determine entity aliveness
+
+    This function determines if an entity is alive or not
+
+    :param entity: A dictionary representing an entity in the game (player or monster)
+    :precondition: entity must be a dictionary that represents either a player or monster
+    :postcondition: The HP of the entity is evaluated and determined if alive or not
+    :return: A boolean True if entity HP is less than or equal to 0, otherwise False
+
+    >>> sample_player = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 1}
+    >>> is_alive(sample_player)
+    True
+
+    >>> sample_player = {'HP': -69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 1}
+    >>> is_alive(sample_player)
+    False
+    """
     if entity['HP'] <= 0:
         return False
     else:
         return True
 
 
-def describe_location(player, board):
+def describe_location(player: dict, board: dict) -> str:
+    """
+    Pull description from board
+
+    This function pulls the description at the player coordinate on the board, and returns it for use
+
+    :param player: A dictionary representing the player character
+    :param board: A dictionary representing the playable area
+    :precondition: player and board be dictionaries must represent the player character and the playable area
+    :precondition: player coordinates must reside within the board's boundaries
+    :postcondition: The description at the player coordinates is pulled
+    :return: A string that represents the description at the player's coordinates
+
+    >>> test_board = {(0, 0, 0): {'Event': 'None', 'Description':  'Wow'}, (1, 0, 0): {'Event': 'None', 'Description': 'Cool'}}
+    >>> sample_player = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 1}
+    >>> describe_location(sample_player, test_board)
+    'Wow'
+
+    >>> test_board = {(0, 0, 0): {'Event': 'None', 'Description':  'Wow'}, (1, 0, 0): {'Event': 'None', 'Description': 'Cool'}}
+    >>> sample_player = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 1, 'Y': 0, 'Z': 0, 'SPD': 1}
+    >>> describe_location(sample_player, test_board)
+    'Cool'
+    """
     description = board[(player['X'], player['Y'], player['Z'])]['Description']
     return description
 
 
-def convert_dictionary(board):
+def convert_dictionary(board: dict[str or tuple]) -> dict[tuple or str]:
+    """
+    Convert dictionary keys between tuple and str
+
+    This function takes the keys of the dictionary representing the playable area, and converts them from str to tuple,
+    or vice versa for use in json saving
+
+    :param board: A dictionary representing the playable area, with either tuple coordinates or a string in
+    tuple-formatted coordinates as keys
+    :precondition: board must be a dictionary representing the playable area, with tuple formatted keys
+    :precondition: board with keys that are type string must be strings that are formatted like tuples
+    :postcondition: Tuples keys are converted to strings with tuple format
+    :postcondition: Strings with tuple formatting are converted to tuples
+    :return: A dictionary with keys of a converted type is returned
+
+    >>> test_board = {(0, 0, 0): {'Event': 'None', 'Description': 'Wow'}}
+    >>> convert_dictionary(test_board)
+    {'(0, 0, 0)': {'Event': 'None', 'Description': 'Wow'}}
+
+    >>> test_board = {'(0, 0, 0)': {'Event': 'None', 'Description': 'Wow'}}
+    >>> convert_dictionary(test_board)
+    {(0, 0, 0): {'Event': 'None', 'Description': 'Wow'}}
+    """
     converted_dictionary = {}
 
     if type(tuple(board.keys())[0]) == tuple:
@@ -150,7 +240,17 @@ def convert_dictionary(board):
     return converted_dictionary
 
 
-def get_saves_list():
+def get_saves_list() -> None | list[str]:
+    """
+    Get save labels in the saves directory
+
+    This function reads all the correctly formatted save files in the saves directory and appends them all to a list
+    containing the numbered labels for each save file.
+
+    :postcondition: All numbered save labels are appended into a list
+    :return: If saves directory does not exist, or directory is empty, return None, otherwise return list of integers
+    typecasted as string type, that represents the number labels of each save
+    """
     file_num = 1
     file_path = os.path.join(os.path.dirname(__file__), 'saves')
 
