addc63d4fe3850e56b5095cd259d1817a813f0aa
Jonathan Liu <jwliu0724@hotmail.com>
2023-12-05 04:50:27 -0800

    add extra test case

--- Unit_Tests/test_chase_player.py
+++ Unit_Tests/test_chase_player.py
@@ -47,3 +47,12 @@ class Test(TestCase):
                     'Alerted': True, 'Alert_Counter': 19}
         chase_player(sample_player, sample_monster)
         self.assertEqual(expected, sample_monster)
+
+    def test_last_chase_cycle(self):
+        sample_player = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 1}
+        sample_monster = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 2, 'Y': 0, 'Z': 0, 'SPD': 2,
+                          'Alerted': True, 'Alert_Counter': 1}
+        expected = {'HP': 69, 'MAX HP': 69, 'ATK': 69, 'DEF': 69, 'X': 0, 'Y': 0, 'Z': 0, 'SPD': 2,
+                    'Alerted': False, 'Alert_Counter': 0}
+        chase_player(sample_player, sample_monster)
+        self.assertEqual(expected, sample_monster)
